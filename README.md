3253fb2a89f531c234d6f9c301640b02ce8f9b08
Marwen Hlaoui <marwenhlaoui@gmail.com>
2016-09-23 11:33:41 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# LoadImg
-jQuery Image load and preview before upload 
+
+#LoadImg
+###Description : 
+LoadImg is JQuery function , you can load and preview picture before upload with ajax , php or any framework .
+###Install : 
+easy to include in your application , just [download](https://github.com/marwenhlaoui/LoadImg/archive/master.zip) or clone from github
+
+```git
+$git clone https://github.com/marwenhlaoui/LoadImg.git 
+```
+###Include:
+add style file `loadimg.min.css` in head
+```html
+<head>
+	...
+	<link rel="stylesheet" type="text/css" href="assets/css/loadimg.min.css">
+	...
+</head>
+```
+
+
+add javascript  files `loadimg.min.js`  and `jquery.min.js`
+```html
+	...
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/loadimg.min.js"></script>
+	...
+```
+###Use : 
+in your form
+```html
+<label id="upload">
+	<input type="file">
+</label>
+```
+
+in your application js 
+```js
+$('#upload').loadImg({});
+```
+###Optional parameters : 
+####Text : 
+```js
+$('#upload').loadImg({
+	"text"	: "Upload Picture ...",
+});
+```
+default text: Upload
+####Image extension : 
+```js
+$('#upload').loadImg({
+	"fileExt" : ["png","jpg"],
+});
+```
+default values : jpg
+
+####Image Size : 
+```js
+$('#upload').loadImg({
+	"fileSize_min"	: 0,//0Mo
+	"fileSize_max"	: 1,//1Mo
+});
+```
+default values : min = 0 Mo and max = 2Mo
+
+### [Demo](http://app.loadimg.marwenhlaoui.me/)
+
+### License :
+
+This Jquery Projects is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
+
+
+----------
+Made with &#9829; by [Marwen Hlaoui](http://marwenhlaoui.me)
---
+++ dist/loadimg.js
@@ -0,0 +1,103 @@
+/**
+ * loadImg 
+ *
+ * Copyright 2016, Marwen Hlaoui - http://http:marwenhlaoui.me 
+ *
+ * Released under the MIT license - http://opensource.org/licenses/MIT
+ */
+
+;(function($){ 
+
+    $.fn.loadImg = function(options){ 
+        // set a reference to our element
+        var def = {
+            "fileExt"  : ['jpg'],
+            "fileSize" : [0,2]
+
+        }; 
+        var el = $(this).selector; 
+        var loader = $(el).append('<div class="loader"></div>');
+        var img = $(el).append('<img>');
+        var text = (typeof options.text === 'undefined') ? 'Upload' : options.text;
+        var init = function(){
+            
+           $(el).addClass("_cssimg");
+
+            $(el).attr("text-file",text);
+            /* load photo */
+            $(el).find('input[type=file]').on('change',function(){
+                $(el).find('.loader').show(); 
+                if((verifExt(this) != false)&&(verifSize(this) != false)){
+                    loadUrl(this);
+                }
+            });
+           
+        }
+        var loadUrl = function(input){
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    setTimeout(showImg(e.target.result) , parseInt(Math.random() * 800 + 800));
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        var showImg = function(url){
+                $(el).find('img').show();
+                $(el).removeClass('_cssimg');
+                $(el).addClass("_imgshow");
+                $(el).find('img').attr('src', url);
+                $(el).find('.loader').hide('slow');
+ 
+        }
+        var verifExt = function(input){
+            if (input.files && input.files[0]) {
+                var f = input.files[0];
+                var validExtensions = ['jpg','gif','png']; //array of valid extensions
+                if (options.fileExt) {
+                    listExtensions = options.fileExt;
+                }else{
+                    listExtensions = def.fileExt;
+                }
+                var fileName = f.name;
+                var fileNameExt = fileName.substr(fileName.lastIndexOf('.') + 1);
+                if (($.inArray(fileNameExt, validExtensions) == -1)||($.inArray(fileNameExt, listExtensions) == -1)){ 
+                   return _error("Invalid file type");
+                }
+            }
+            
+        }
+        var verifSize = function(input){ 
+
+            if (input.files && input.files[0]) {
+                var f = input.files[0];
+                var file_size =  parseInt(0); 
+                var min = def.fileSize[0];
+                var max = def.fileSize[1];
+                var convert = parseInt(1053633);
+                //convert mo -> oct
+                //mo * 1053633 
+                if (options.fileSize_min) {min = options.fileSize_min}
+                if (options.fileSize_max) {max = options.fileSize_max} 
+                if (f.size) {file_size = f.size}
+                if ((f.size < parseInt(min * convert))||(f.size > parseInt(max * convert))) {
+                     return _error("Invalid file size");
+                } 
+            }
+            
+        }
+        var _error = function(error_type){
+            $(el).find('img').hide();
+            $(el).find('.loader').hide('slow');
+            $(el).removeClass("_imgshow");
+            $(el).addClass("_cssimg");
+            /*Invalid file type*/
+           return false;
+        }
+
+        init();
+ 
+    }
+
+})(jQuery);
+ 
